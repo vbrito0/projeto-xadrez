2b4b0bd626f2c9751b6de699e0bd49032989cb88
Victor Brito <victor.silva1189@gmail.com>
2024-08-14 20:17:30 -0300

    Movimentos possíveis de uma peça

--- src/xadrez/pecas/Torre.java
+++ src/xadrez/pecas/Torre.java
@@ -14,4 +14,10 @@ public class Torre extends PecaXadrez{
 	public String toString() {
 		return "T";
 	}
+
+	@Override
+	public boolean[][] possiveisMovimentos() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		return mat;
+	}
 }
